981205356666f1f11e20b757c8722c6f63a5ce2f
Ana López <anamuyamiga@gmail.com>
2023-05-05 10:40:21 +0200

    clase barbero inicio

--- Barbero.py
+++ Barbero.py
@@ -11,3 +11,9 @@ barbero_durmiendo = Semaphore(0)
 cliente = Semaphore(0)
 cliente_espera = Semaphore(0)
 cliente_atendido = Semaphore(0)
+
+class Barbero(Thread):
+    def __init__(self):
+        super().__init__()
+        self.estado = False #Es false si está durmiendo. True si está despierto
+        
