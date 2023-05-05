deeb16c33dbcf6a382f36b5f351293e111ec745b
Ana López <anamuyamiga@gmail.com>
2023-05-05 11:30:03 +0200

    run cliente

--- Barbero.py
+++ Barbero.py
@@ -7,7 +7,7 @@ num_clientes = 8
 num_ocupadas = 0
 
 #Creo semáforos cerrados para controlar las posibles situaciones del barbero y los clientes.
-barbero_durmiendo = Semaphore(0)
+barbero_ocupado = Semaphore(0)
 cliente = Semaphore(0)
 cliente_espera = Semaphore(0)
 cliente_atendido = Semaphore(0)
@@ -31,14 +31,14 @@ class Barbero(Thread):
                 '''Como el barbero está durmiendo, espero hasta que entren clientes para despertarlo.
                 Eso significa que se va a quedar bloqueado en esta linea hasta que en la clase clientes se despierte al barbero desbloqueando el semáforo clientes'''
 
-            barbero_durmiendo.release() #Llego a esta linea cuando se ha despertado al barbero. Desboqueo su semáforo
+            barbero_ocupado.release() #Llego a esta linea cuando el barbero no está haciendo nada (pero no está durmiendo). Desboqueo su semáforo
             cliente_espera.acquire() #Lo desbloqueo cuando esté siendo atendido (en la otra clase)
 
             print(f"El barbero está atendiendo al cliente\n")
             time.sleep(random.randint(1, 2))
             cliente_atendido.release() #Soltamos el semáforo cuando el cliente ha termiando de estar atendido
             print(f"El cliente ha sido atendido, se va\n")
-            
+
 
 class Cliente(Thread):
     #Los distintos estados del cliente son los semáforos de arriba. 
@@ -57,7 +57,31 @@ class Cliente(Thread):
         self.id = id
 
     def run(self):
-        pass
+        global num_ocupadas
+        if num_ocupadas == num_sillas:
+            print("El cliente se va al no haber sillas libres")
+        else: 
+            if barbero.estado == False: #Si el barbero está durmiendo
+                cliente.release() #Liberamos el semáforo cuando entra un cliente
+                print(f"El cliente número {self.id} está siendo atendido")
+                num_ocupadas += 1 #Aumentamos en 1 el numero de sillas ocupadas
+                barbero.set_estado(True) #Despertamos al barbero
+                barbero_ocupado.acquire() #Bloqueo el semáforo ya que le barbero está con un cliente
+
+            else: #Es igual pero no despertamos al barbero porque ya está despierto
+                num_ocupadas += 1 
+                print(f"El cliente num {self.id} espera a ser atendido en una silla")
+                barbero_ocupado.acquire()
+
+            print(f"El cliente num {self.id} está siendo atendido")
+            num_ocupadas -= 1 #El cliente se va, bajamos en 1 las sillas ocupadas
+            cliente_espera.release()
+            cliente_atendido.acquire()
+            print(f"El cliente num {self.id} se va.")
+
+            if num_ocupadas == 0:
+                print("El barbero se duerme")
+                barbero.set_estado(False)
 
 
 #CÓDIGO PRINCIPAL
