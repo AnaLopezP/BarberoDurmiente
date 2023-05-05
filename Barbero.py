5d81767aebfbf8aa8bd39efb64cddcc7c3f1ddd7
Ana López <anamuyamiga@gmail.com>
2023-05-05 10:49:12 +0200

    setter y getter

--- Barbero.py
+++ Barbero.py
@@ -35,3 +35,10 @@ class Cliente(Thread):
     def _init_(self, id):
         super().__init__()
         self.id = id
+
+    def get_id(self):
+        return self.id
+
+    def set_id(self, id):
+        self.id = id
+        
